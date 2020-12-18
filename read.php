33f299e9c7fd355f8669a6e989a5f822d3120370
Dmitri Popov <dmpop@linux.com>
2020-12-18 14:03:20 +0100

    Favicon

--- read.php
+++ read.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <title>箱 Read</title>
     <meta charset="utf-8">
-    <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🗃️</text></svg>">
+    <link rel="icon" href="favicon.png">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="water.css" />
 </head>
